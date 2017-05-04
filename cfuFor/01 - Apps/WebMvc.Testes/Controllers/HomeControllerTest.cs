404c3c8665cb417e270803ca246f736dcfd5d597
Wilson Mellado <wilson@er7.com.br>
2017-05-04 01:32:33 -0300

    Testes

--- cfuFor/01 - Apps/WebMvc.Testes/Controllers/HomeControllerTest.cs
+++ cfuFor/01 - Apps/WebMvc.Testes/Controllers/HomeControllerTest.cs
@@ -1,49 +1,49 @@
-﻿//using System.Web.Mvc;
-//using NUnit.Framework;
-//using WebMvc.Controllers;
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using WebMvc.Controllers;
 
-//namespace WebMvc.Testes.Controllers
-//{
-//    [TestFixture]
-//    public class HomeControllerTest
-//    {
-//        [Test]
-//        public void Index()
-//        {
-//            // Arrange
-//            HomeController controller = new HomeController();
+namespace WebMvc.Testes.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
 
-//            // Act
-//            ViewResult result = controller.Index() as ViewResult;
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
 
-//            // Assert
-//            Assert.IsNotNull(result);
-//        }
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-//        [Test]
-//        public void About()
-//        {
-//            // Arrange
-//            HomeController controller = new HomeController();
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
 
-//            // Act
-//            ViewResult result = controller.About() as ViewResult;
+            // Act
+            ViewResult result = controller.About() as ViewResult;
 
-//            // Assert
-//            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-//        }
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
 
-//        [Test]
-//        public void Contact()
-//        {
-//            // Arrange
-//            HomeController controller = new HomeController();
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
 
-//            // Act
-//            ViewResult result = controller.Contact() as ViewResult;
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
 
-//            // Assert
-//            Assert.IsNotNull(result);
-//        }
-//    }
-//}
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
